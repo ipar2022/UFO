c2e1c3490736375bc697aedbf890fc9199c85370
ipar2022 <parnetwork@outlook.com>
2023-02-02 22:33:32 -0600

    Adding Module 12 Challenge.

---
+++ static/css/images/js/app_1.js
@@ -0,0 +1,41 @@
+const myName = 'James';
+
+console.log('Hello World');
+console.log(myName);
+
+
+
+
+
+
+
+
+//<div class="header">
+//  <h1>Header</h1>
+//  <p>My supercool header</p>
+//</div>
+
+// import the data from data.js
+//const tableData = data;
+// Reference the HTML table using d3
+//var tbody = d3.select("tbody");
+// Simple JavaScript console.log statement
+//function printHello() {
+//    console.log("Hello there!");
+//  }
+//  var y = 2;
+//  console.log(y);
+
+
+
+  // Takes two numbers and adds them
+//function addition(a, b) {
+//  return a + b;
+//  console.log(a+b);
+//}
+
+// Functions can call other functions
+//function doubleAddition(c, d) {
+//  var total = addition(c, d) * 2;
+//  return total;
+//}
